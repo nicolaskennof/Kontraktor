8e7eeff4c1cf669e70b69a58e7048a732bac047e
froylan <froylan.vargas.gomez>
2019-01-22 22:00:50 -0600

    POC UploadImages working

--- controllers/pocImageController.js
+++ controllers/pocImageController.js
@@ -4,30 +4,37 @@ var fs = require("fs");
 
 module.exports = {
     create: (req, res) => {
-        var filesrc = path.join(__dirname, "../client/public/assets/img/tempProfileImages/blondi.jpg");
+        var filesrc = path.join(__dirname, "../", req.file.path);
+
         //Modify this to use multer. 
-        var bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'profileImages' });
+        var bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'fs' });
         fs.createReadStream(filesrc)
-            .pipe(bucket.openUploadStream('myFile.jpg'))
+            .pipe(bucket.openUploadStream(req.file.originalname))
             .on('error', () => {
                 console.log("Some error occured:" + error);
-                res.send(error);
+                res.json(error);
             })
             .on('finish', (result) => {
                 console.log(result);
-                res.send('Done Uploading');
+                res.json('Done Uploading');
             });
+    },
+
+    getImage: (req, res) => {
+        
+        var db = mongoose.connection.db;
+        
+        var chunksCollection = db.collection('fs.chunks');
+        var filesCollection = db.collection('fs.files');
+        chunksCollection.find({ _id: new mongoose.mongo.ObjectID(req.params.id) }).toArray().then(chunks => {
+            filesCollection.find({_id : new mongoose.mongo.ObjectID(chunks[0].files_id)}).toArray().then(files => {
+                res.set('Content-Type', "image/*");
+                res.set('Content-Disposition', 'attachment; filename="' + files[0].filename + '"');
+                res.send(chunks[0].data.buffer);
+            })
+        })
+
+
+
     }
 }
-
-
-/*
-
-    <form action="/upload/photo" enctype="multipart/form-data" method="POST">
-  <input type="file" name="myImage" accept="image/*" />
-  <input type="submit" value="Upload Photo"/>
-</form>
-
-
-
-*/
