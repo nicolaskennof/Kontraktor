32fcc0c17e9d5c493a75b40a0a0191ef90d5f8fd
LPjavier <42053940+lugof@users.noreply.github.com>
2019-02-05 20:37:06 -0600

    rutas api front end

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -12,5 +12,45 @@ export default {
   createKontratado: function(kontratado){
     return axios.post("/api/kontratado/", kontratado);
   },
+  insertCostRate: function(costRate){
+    return axios.post("/api/costRate/",costRate);
+  },
+  editCostRate: function(id,costRate){
+    return axios.post("/api/costRate/"+id, costRate);
+  },
+  deleteCostRate: function(id){
+    return axios.delete("/api/costRate/delete/"+id);
+  },
+  insertFavourite: function(id,favourite){
+    return axios.post("/api/favourite/", favourite);
+  },
+  deleteFavourite: function(id){
+    return axios.delete("/api/favourite/delete/"+ id);
+  },
+  insertHire: function(hire){
+    return axios.post("/api/hire/", hire);
+  },
+  insertMessage: function(message){
+    return axios.post("/api/message/", message);
+  },
+  insertQualityRate: function(quality){
+    return axios.post("/api/qualityRate/", quality);
+  },
+  editQualityRate: function(id, quality){
+    return axios.post("/api/qualityRate/"+id, quality);
+  },
+  deleteQualityRate: function(id){
+    return axios.post("/api/qualityRate/delete/"+id);
+  },
+  insertReview: function(review){
+    return axios.post("/api/review/", review);
+  },
+  editReview: function(id, review){
+    return axios.post("/api/review/"+id, review);
+  },
+  deleteReview: function(id){
+    return axios.delete("/api/review/delete/"+id);
+  }
 
+  
 };
