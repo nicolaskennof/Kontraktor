135d33c602f47808e21956c5dd50262b71a0e552
Nicolas Kennof <nicolas.kennof@gmail.com>
2019-02-10 00:02:55 -0600

    carousel fixed

--- client/src/Components/carousel/carousel.js
+++ client/src/Components/carousel/carousel.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
 import './style.css'
+import Carpenter from "../../img/carpenter.jpg"
+import Electrician from "../../img/electrician.jpg"
+import Cleaner from "../../img/cleaner.jpg"
 
-function ImgCarousel(props) {
+
+function ImgCarousel() {
     return (
 
         <div className="carouselContainer">
@@ -10,33 +14,33 @@ function ImgCarousel(props) {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="assets/img/carousel/retapizCarousel.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                       
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="assets/img/carousel/carpinteriaCarousel.jpg"
+                        src={Electrician}
                         alt="Second slide"
                     />
 
                     <Carousel.Caption>
-                       
+
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="assets/img/carousel/electricosCarousel.jpg"
+                        src={Carpenter}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={Cleaner}
                         alt="Third slide"
                     />
 
                     <Carousel.Caption>
-                       
+
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
